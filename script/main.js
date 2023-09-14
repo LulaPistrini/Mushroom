f0531a111d7488f98ddedd304e139f87f485f8af
LulaPistrini <pistrini.luciana@gmail.com>
2023-09-13 23:25:43 -0300

    Mi primer commit

---
+++ script/main.js
@@ -0,0 +1,41 @@
+
+//Usar for - while - if/else - switch
+
+//Nombre del usuario y bienvenida.
+
+let nombreUsuario = prompt("¿Cual es tu nombre?");
+
+alert ("🍄 " + nombreUsuario + " te damos la bienvenida a Mush-Room 🍄");
+
+let edad = parseInt(prompt("¿Cuantos años tenes?"));
+
+if ( edad >= 18) {
+    alert("Podes acceder a nuestros productos");
+} else {
+  alert("Esta pagina es para mayores de edad");
+ } // Como podria hacer para que no acceda a la pagina en caso de ser menor?
+
+ // Crear Usuario y Contraseña
+
+
+ let usuario = prompt("Elija un mail como usuario:");
+ let contraseña = prompt("Elija una contraseña (Dene tener hasta 6 digitos):");
+
+
+
+// CALCULAR EL INTERES EN LAS CUOTAS ELEGIDAS POR EL USUARIO
+
+    const CUOTAS = 12;
+    const BLACK = 4000;
+    const CANTHARELLUS = 5000;
+    const LEON = 4200;
+
+
+
+
+
+
+
+
+
+
